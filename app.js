0ce48fc87624f91c9dd70ad8e32daf6107dc6c08
Andrii Melnykov <andy.melnikov@gmail.com>
2019-06-23 22:46:47 +0300

    chore(foss): sync app.js to work on new socketstream support

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var ss = require('socketstream')
+console.error("appJs")
+var	ss = require('socketstream')
+,	fs = require('fs')
+,   express = require('express')
+var bodyParser = require('body-parser')
+var morgan = require('morgan')
+
+function openForAppend(fileName)
+{
+	return fs.createWriteStream(fileName, {'flags': 'a'})
+}
+
 
 ss.client.define('main', {
 	view:   'app.jade',
@@ -7,14 +18,44 @@ ss.client.define('main', {
 	tmpl:   ['main']
 })
 
+ss.client.set({liveReload: false})
+
+// Remove to use only plain .js, .html and .css files if you prefer
 ss.client.formatters.add(require('ss-coffee'))
 ss.client.formatters.add(require('ss-jade'))
 ss.client.formatters.add(require('ss-stylus'))
+/*
+ss.session.store.use('redis');
+ss.publish.transport.use('redis');
+*/
+ss.ws.transport.use('engineio', { io: function(io) {
+   var m = 3
+   io.set('close timeout', 25 * m)
+   io.set('heartbeat timeout', 15 * m)
+   io.set('heartbeat interval', 20 * m)
+}})
 
-ss.http.route('/', function(req, res){
-	res.serveClient('main')
-})
+// Minimise and pack assets if you type SS_ENV=production node app
+if (ss.env == 'production') ss.client.packAssets();
 
-if (ss.env === 'production') ss.client.packAssets()
+function routes(app)
+{
+/*
+	app.get('/billing/blockchain/notify', billingBlockchain.notify(user.onPayment))
+	app.get('/billing/blockchain2/notify', billingBlockchain2.notify(user.onPayment))
+*/
+	app.get('/', function (req, res)
+	{
+		res.serveClient('main')
+	})
+
+}
+
+ss.http.middleware.prepend(morgan('combined', { stream: openForAppend("var/log/http.log")}))
+ss.http.middleware.append(bodyParser())
+
+const router = express.Router()
+routes(router)
+ss.http.middleware.append(router)
 
 ss.start()
